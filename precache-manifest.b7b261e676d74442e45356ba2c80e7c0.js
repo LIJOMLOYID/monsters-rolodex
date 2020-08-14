8df3a9b791fca90138b2d34dc689c31477c588d0
LIJOMLOYID <lijomloyid@gmail.com>
2020-08-14 18:29:34 +0530

    Updates

---
+++ precache-manifest.b7b261e676d74442e45356ba2c80e7c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce359240c0490b20871bce52be62d1c3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f06bcde8a86e611c33d7",
+    "url": "/monsters-rolodex/static/css/main.ae1cb90b.chunk.css"
+  },
+  {
+    "revision": "d646dc6f66e2f6700246",
+    "url": "/monsters-rolodex/static/js/2.98ed97f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.98ed97f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f06bcde8a86e611c33d7",
+    "url": "/monsters-rolodex/static/js/main.5290b5ea.chunk.js"
+  },
+  {
+    "revision": "68f38743d52cf3d6b91c",
+    "url": "/monsters-rolodex/static/js/runtime-main.33e93d66.js"
+  }
+]);
